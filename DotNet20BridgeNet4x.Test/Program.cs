1c93e8394862d036b9101848e35290a32515b687
andyhebear <andyhebear@163.com>
2022-08-18 20:48:25 +0800

    add test project

---
+++ DotNet20BridgeNet4x.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNet20BridgeNet4x.Test
+{
+    class Program
+    {
+        static void Main(string[] args) {
+        }
+    }
+}
