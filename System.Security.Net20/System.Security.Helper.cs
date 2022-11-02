112ff9c014b0dafc2cbef28cf7076edf28068732
andyhebear <andyhebear@163.com>
2022-11-02 15:19:57 +0800

    System.Security.Helper

---
+++ System.Security.Net20/System.Security.Helper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Security
+{
+	//public delegate TResult Func<T, TResult>(T arg);
+	[AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
+	public class MonoTODOAttribute : Attribute
+	{
+
+		string comment;
+
+		public MonoTODOAttribute() {
+		}
+
+		public MonoTODOAttribute(string comment) {
+			this.comment = comment;
+		}
+
+		public string Comment {
+			get { return comment; }
+		}
+	}
+}
