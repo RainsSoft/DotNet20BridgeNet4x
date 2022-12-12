c2dc9fc040fe14ab9f47c1eb8a4ed5f06c3e317b
andyhebear <andyhebear@163.com>
2022-12-12 11:11:45 +0800

    Create System.Numerics.Helper.cs

---
+++ System.Numerics.Net20/System.Numerics.Helper.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+#if NET_4_0
+
+using System;
+
+namespace System.Diagnostics.Contracts
+{
+	[ConditionalAttribute("CONTRACTS_FULL")]
+	[AttributeUsageAttribute (AttributeTargets.Class | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Event | AttributeTargets.Parameter | AttributeTargets.Delegate)]
+	internal sealed class PureAttribute : Attribute
+	{
+	}
+}
+
+#endif
