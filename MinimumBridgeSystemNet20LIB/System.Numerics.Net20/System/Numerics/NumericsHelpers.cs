425223dbb8c72f297fe6049657ba6d93de804f08
andyhebear <andyhebear@163.com>
2022-12-21 12:05:01 +0800

    move System.Numerics.Net20 project file

--- MinimumBridgeSystemNet20LIB/System.Numerics.Net20/System/Numerics/NumericsHelpers.cs
+++ MinimumBridgeSystemNet20LIB/System.Numerics.Net20/System/Numerics/NumericsHelpers.cs
@@ -356,5 +356,212 @@ namespace System.Numerics
         //      uu = (uu & 0x0000FFFF0000FFFF) + ((uu >> 16) & 0x0000FFFF0000FFFF);
         //      return (int)((uint)uu + (uint)(uu >> 32));
         //    }
+
+        //[DebuggerNonUserCode]
+        public static int Log2(int number) {
+            if (number < 0) {
+                throw new ArgumentOutOfRangeException(nameof(number), "The logarithm of a negative number is imaginary.");
+            }
+
+            return Log2(unchecked((uint)number));
+        }
+
+        //[DebuggerNonUserCode]
+        public static int Log2(long number) {
+            if (number < 0) {
+                throw new ArgumentOutOfRangeException(nameof(number), "The logarithm of a negative number is imaginary.");
+            }
+
+            return Log2(unchecked((ulong)number));
+        }
+
+        [CLSCompliant(false)]
+        //[DebuggerNonUserCode]
+        public static int Log2(uint number) {
+            if (number == 0) {
+                throw new ArgumentOutOfRangeException(nameof(number), "The logarithm of zero is not defined.");
+            }
+
+            number |= number >> 1;
+            number |= number >> 2;
+            number |= number >> 4;
+            number |= number >> 8;
+            number |= number >> 16;
+            return PopulationCount(number >> 1);
+        }
+
+        [CLSCompliant(false)]
+        //[DebuggerNonUserCode]
+        public static int Log2(ulong number) {
+            if (number == 0) {
+                throw new ArgumentOutOfRangeException(nameof(number), "The logarithm of zero is not defined.");
+            }
+
+            number |= number >> 1;
+            number |= number >> 2;
+            number |= number >> 4;
+            number |= number >> 8;
+            number |= number >> 16;
+            number |= number >> 32;
+            return PopulationCount(number >> 1);
+        }
+        public static int PopulationCount(int value) {
+            unchecked {
+                return PopulationCount((uint)value);
+            }
+        }
+
+        public static int PopulationCount(long value) {
+            unchecked {
+                return PopulationCount((ulong)value);
+            }
+        }
+
+        // Gem from Hacker's Delight
+        // Returns the number of bits set in @value
+        [CLSCompliant(false)]
+        public static int PopulationCount(uint value) {
+            value -= (value >> 1) & 0x55555555;
+            value = (value & 0x33333333) + ((value >> 2) & 0x33333333);
+            value = (value + (value >> 4)) & 0x0F0F0F0F;
+            value += value >> 8;
+            value += value >> 16;
+            return (int)(value & 0x0000003F);
+        }
+
+        // Based on code by Zilong Tan on Ulib released under MIT license
+        // Returns the number of bits set in @x
+        [CLSCompliant(false)]
+        public static int PopulationCount(ulong value) {
+            value -= (value >> 1) & 0x5555555555555555UL;
+            value = (value & 0x3333333333333333UL) + ((value >> 2) & 0x3333333333333333UL);
+            value = (value + (value >> 4)) & 0x0f0f0f0f0f0f0f0fUL;
+            return (int)((value * 0x0101010101010101UL) >> 56);
+        }
+
+        public static int BinaryReverse( int value) {
+            unchecked {
+                return (int)BinaryReverse((uint)value);
+            }
+        }
+
+        public static long BinaryReverse( long value) {
+            unchecked {
+                return (long)BinaryReverse((ulong)value);
+            }
+        }
+
+        [CLSCompliant(false)]
+        public static uint BinaryReverse( uint value) {
+            value = ((value & 0xaaaaaaaa) >> 1) | ((value & 0x55555555) << 1);
+            value = ((value & 0xcccccccc) >> 2) | ((value & 0x33333333) << 2);
+            value = ((value & 0xf0f0f0f0) >> 4) | ((value & 0x0f0f0f0f) << 4);
+            value = ((value & 0xff00ff00) >> 8) | ((value & 0x00ff00ff) << 8);
+            return (value >> 16) | (value << 16);
+        }
+
+        [CLSCompliant(false)]
+        public static ulong BinaryReverse( ulong value) {
+            GetParts(value, out var lo, out var hi);
+            return BuildUInt64(BinaryReverse(lo), BinaryReverse(hi));
+        }
+
+        [CLSCompliant(false)]
+        public static ulong BuildUInt64(uint hi, uint lo) {
+            return ((ulong)hi << 32) | lo;
+        }
+        public static int SingleAsInt32(float value) {
+            return BitConverter.ToInt32(BitConverter.GetBytes(value), 0);
+        }
+
+        [CLSCompliant(false)]
+        public static uint SingleAsUInt32(float value) {
+            unchecked {
+                return (uint)BitConverter.ToInt32(BitConverter.GetBytes(value), 0);
+            }
+        }
+        #region
+      
+
+        public static void GetParts(double value, out int sign, out long mantissa, out int exponent, out bool finite) {
+            GetDoubleParts(value, out sign, out exponent, out var tmpMantissa, out finite);
+            mantissa = (long)tmpMantissa;
+        }
+
+        public static void GetParts(float value, out int sign, out int mantissa, out int exponent, out bool finite) {
+            if (value.CompareTo(0.0f) == 0) {
+                sign = 0;
+                mantissa = 0;
+                exponent = 1;
+                finite = true;
+            }
+            else {
+                var bits = SingleAsInt32(value);
+                sign = bits < 0 ? -1 : 1;
+                exponent = (bits >> 23) & 0xff;
+                if (exponent == 2047) {
+                    finite = false;
+                    mantissa = 0;
+                }
+                else {
+                    finite = true;
+                    mantissa = bits & 0xffffff;
+                    if (exponent == 0) {
+                        // Subnormal numbers; exponent is effectively one higher,
+                        // but there's no extra normalization bit in the mantissa
+                        exponent = 1;
+                    }
+                    else {
+                        // Normal numbers; leave exponent as it is but add extra
+                        // bit to the front of the mantissa
+                        mantissa |= 1 << 23;
+                    }
+
+                    // Bias the exponent. It's actually biased by 127, but we're
+                    // treating the mantissa as m.0 rather than 0.m, so we need
+                    // to subtract another 23 from it.
+                    exponent -= 150;
+                    if (mantissa == 0) {
+                        return;
+                    }
+
+                    while ((mantissa & 1) == 0) {
+                        mantissa >>= 1;
+                        exponent++;
+                    }
+                }
+            }
+        }
+
+        public static void GetParts(int value, out short lo, out short hi) {
+            unchecked {
+                lo = (short)value;
+                hi = (short)(value >> 16);
+            }
+        }
+
+        public static void GetParts(long value, out int lo, out int hi) {
+            unchecked {
+                lo = (int)value;
+                hi = (int)((ulong)value >> 32);
+            }
+        }
+
+        [CLSCompliant(false)]
+        public static void GetParts(uint value, out ushort lo, out ushort hi) {
+            unchecked {
+                lo = (ushort)value;
+                hi = (ushort)(value >> 16);
+            }
+        }
+
+        [CLSCompliant(false)]
+        public static void GetParts(ulong value, out uint lo, out uint hi) {
+            unchecked {
+                lo = (uint)value;
+                hi = (uint)(value >> 32);
+            }
+        }
+        #endregion
     }
 }
---
+++ MinimumBridgeSystemNet20LIB/System.Numerics.Net20/System/NumericsEx/BigNumberBuffer.cs
@@ -0,0 +1,28 @@
+﻿#if !LARGETHAN_NET40 
+//#if LESSTHAN_NET40 || NETSTANDARD1_0
+
+using System.Text;
+
+namespace System.Numerics
+{
+    internal sealed class BigNumberBuffer
+    {
+        public readonly StringBuilder Digits;
+
+        private BigNumberBuffer(StringBuilder digits)
+        {
+            Digits = digits;
+        }
+
+        public bool Negative { get; set; }
+
+        public int Scale { get; set; }
+
+        public static BigNumberBuffer Create()
+        {
+            return new BigNumberBuffer(new StringBuilder());
+        }
+    }
+}
+
+#endif
