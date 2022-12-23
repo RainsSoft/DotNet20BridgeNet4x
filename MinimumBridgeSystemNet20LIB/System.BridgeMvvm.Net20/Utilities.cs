39e0705b0e9c756a6cb29f7fc73ecffe1f75fbf3
andyhebear <andyhebear@163.com>
2022-12-23 15:50:44 +0800

    System.BridgeMvvm.Net20.csproj

---
+++ MinimumBridgeSystemNet20LIB/System.BridgeMvvm.Net20/Utilities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.BridgeMvvm
+{
+    /// <summary>
+    /// https://github.com/OrangeCube/MinimumAsyncBridge
+    /// </summary>
+    public class Utilities
+    {
+    }
+}
