8d56b118e9f8ad9b75a520528a8191a0341c1e45
andyhebear <andyhebear@163.com>
2022-12-23 15:44:04 +0800

    MvvmBridge.TypeForward.csproj

---
+++ MinimumBridgeSystemNet20LIB/System.BridgeMvvm.Net20/MvvmBridge.TypeForward/TypeForward.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedTo(typeof(System.Collections.Specialized.INotifyCollectionChanged))]
+[assembly: TypeForwardedTo(typeof(System.Collections.Specialized.NotifyCollectionChangedAction))]
+[assembly: TypeForwardedTo(typeof(System.Collections.Specialized.NotifyCollectionChangedEventArgs))]
+[assembly: TypeForwardedTo(typeof(System.Collections.Specialized.NotifyCollectionChangedEventHandler))]
+[assembly: TypeForwardedTo(typeof(System.Collections.ObjectModel.ObservableCollection<>))]
+[assembly: TypeForwardedTo(typeof(CallerFilePathAttribute))]
+[assembly: TypeForwardedTo(typeof(CallerLineNumberAttribute))]
+[assembly: TypeForwardedTo(typeof(CallerMemberNameAttribute))]
