413647261a2195d5f6db10c2ce4323691177b826
andyhebear <andyhebear@163.com>
2022-12-26 09:25:33 +0800

    Create DataContractIgnoreAttribute.cs

---
+++ MinimumBridgeOtherNet20LIB/Mono.Mathematics.Net20/Attributes/DataContractIgnoreAttribute.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+
+
+namespace Mono.Core
+{
+    [AttributeUsage(AttributeTargets.Delegate | AttributeTargets.Enum | AttributeTargets.Struct | AttributeTargets.Class, Inherited = false)]
+    public class DataContractIgnoreAttribute : Attribute
+    {
+    }
+}
