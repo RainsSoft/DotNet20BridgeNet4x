5e75a0396fca728f6aaa9dd8bc43b164f8c5d26a
andyhebear <andyhebear@163.com>
2022-12-26 09:25:40 +0800

    Create DataMemberIgnoreAttribute.cs

---
+++ MinimumBridgeOtherNet20LIB/Mono.Mathematics.Net20/Attributes/DataMemberIgnoreAttribute.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace Mono.Core
+{
+    /// <summary>
+    /// When specified on a property or field, it will not be used when serializing/deserializing.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, Inherited = false)]
+    public class DataMemberIgnoreAttribute : Attribute
+    {
+    }
+}
