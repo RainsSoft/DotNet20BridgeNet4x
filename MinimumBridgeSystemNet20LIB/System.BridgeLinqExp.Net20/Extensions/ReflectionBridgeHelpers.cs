bc3b4bdc071dcfa9712853f7c361fe08ad2dbc7e
andyhebear <andyhebear@163.com>
2023-01-05 20:17:16 +0800

    Create ReflectionBridgeHelpers.cs

---
+++ MinimumBridgeSystemNet20LIB/System.BridgeLinqExp.Net20/Extensions/ReflectionBridgeHelpers.cs
@@ -0,0 +1,33 @@
+﻿#define NET20
+using System;
+using System.Reflection;
+#if REFLECTIONBRIDGE
+using System.Collections.Generic;
+using System.Linq;
+#endif
+
+namespace System.BridgeLinqExp.Reflection
+{
+    public static class ReflectionBridgeHelper
+    {
+        public static bool IsNullableEnum(this Type type)
+        {
+            return type.IsGenericType() && type.GetGenericTypeDefinition() == typeof(Nullable<>) && type.GetGenericArguments()[0].IsEnum();
+        }
+
+        public static Type UnwrapNullable(this Type type)
+        {
+            if (!type.IsGenericType())
+            {
+                return type;
+            }
+
+            if (type.GetGenericTypeDefinition() != typeof(Nullable<>))
+            {
+                return type;
+            }
+
+            return type.GetGenericArguments()[0];
+        }
+    }
+}
