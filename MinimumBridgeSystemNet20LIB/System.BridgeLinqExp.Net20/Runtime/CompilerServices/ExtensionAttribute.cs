f81764ec25eb8ced073cdc8ac348261f5edc2288
andyhebear <andyhebear@163.com>
2023-01-05 20:16:38 +0800

    System.BridgeLinqExp.Net20\Runtime\CompilerServices

---
+++ MinimumBridgeSystemNet20LIB/System.BridgeLinqExp.Net20/Runtime/CompilerServices/ExtensionAttribute.cs
@@ -0,0 +1,12 @@
+﻿#if !LARGETHAN_NET35
+
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Method)]
+    public sealed class ExtensionAttribute : Attribute
+    {
+        // Empty
+    }
+}
+
+#endif
