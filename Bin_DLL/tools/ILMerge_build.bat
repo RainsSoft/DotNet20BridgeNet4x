2a73adacfbf941e2dfce82102136b294c3d75be2
andyhebear <andyhebear@163.com>
2023-01-10 20:07:30 +0800

    add ILMerge / ILRepack tool

---
+++ Bin_DLL/tools/ILMerge_build.bat
@@ -0,0 +1,4 @@
+ilmerge.exe /?
+ilmerge.exe /log:merge1.log /t:library /targetplatform:v2 /ndebug /out:DotNet20BridgeNet4x.dll.new.dll DotNet20BridgeNet4x.dll System.Mono.Posix.Net20.dll System.Core.Net20.dll System.Threading.Net20.dll
+
+pause
---
+++ Bin_DLL/tools/ILRepack_build.bat
@@ -0,0 +1,3 @@
+ILRepack2.1.8.0.exe /help
+ILRepack2.1.8.0.exe /log:merge2.log /ndebug /wildcards /out:..\..\Bin_DLL\DotNet20BridgeNet4x.dll /target:library DotNet20BridgeNet4x.dll System.Mono.Posix.Net20.dll System.Core.Net20.dll System.Threading.Net20.dll
+pause
