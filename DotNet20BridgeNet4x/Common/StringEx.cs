dbdc61d6a2a39f53e2afd04d5e6ca5b740b7803b
andyhebear <andyhebear@163.com>
2023-01-30 09:31:15 +0800

    Create StringEx.cs

---
+++ DotNet20BridgeNet4x/Common/StringEx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNet20BridgeNet4x
+{
+    public static class StringEx
+    {
+        /// <summary>
+        /// 字符串前后加双引号
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string Quote(this string s) {
+            return "\"" + s+"\"";
+        }
+   
+    }
+}
