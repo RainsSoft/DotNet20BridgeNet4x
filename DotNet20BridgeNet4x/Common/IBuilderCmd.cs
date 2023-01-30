b8ff6b317738c3eba77cbce9ee014c4eab30c743
andyhebear <andyhebear@163.com>
2023-01-30 09:31:13 +0800

    Create IBuilderCmd.cs

---
+++ DotNet20BridgeNet4x/Common/IBuilderCmd.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DotNet20BridgeNet4x
+{
+    public interface IBuilderCmd
+    {
+       
+        /// <summary>
+        /// 构建命令
+        /// </summary>
+        /// <returns></returns>
+        string BuilderCmd(FileInfo outputAssemblyPath, FileInfo primaryAssemblyPath,
+            IEnumerable<FileInfo> assemblyPaths);
+
+    }
+}
