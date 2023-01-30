623bffe8418fe6db73a501ae9eb8b20887440dc6
andyhebear <andyhebear@163.com>
2023-01-30 09:31:26 +0800

    Create TargetPlatformVersion.cs

---
+++ DotNet20BridgeNet4x/Common/TargetPlatformVersion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace DotNet20BridgeNet4x
+{
+    /// <summary>
+    /// Represents the .NET Framework for the target assembly.
+    /// </summary>
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public enum TargetPlatformVersion
+    {
+        /// <summary>
+        /// NET Framework v1
+        /// </summary>
+        v1,
+
+        /// <summary>
+        /// NET Framework v1.1
+        /// </summary>
+        v11,
+
+        /// <summary>
+        /// NET Framework v2
+        /// </summary>
+        v2,
+
+        /// <summary>
+        /// NET Framework v4
+        /// </summary>
+        v4
+    }
+}
