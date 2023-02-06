92fc48d0c85552d6fc41245f7dba1ce84ba757eb
andyhebear <andyhebear@163.com>
2023-02-06 15:26:02 +0800

    Create 01_TaskWithResult.cs

---
+++ DotNet20BridgeNet4x.Test/01_TaskWithResult.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+
+namespace Concurrency
+{
+    public class TaskWithResult : Example
+    {
+        public override int Id => 1;
+
+        public override Task RunAsync()
+        {
+            PrintWithThread("main method");
+
+            var taskWithoutResult = Task.Run(() =>
+            {
+                PrintWithThread("Inside task 1");
+            });
+
+            /*var task = new Task<int>(() => 3);
+            task.Start();*/
+
+            var taskWithResult = Task.Run<int>(() =>
+            {
+                PrintWithThread("Inside task 2");
+                return 3;
+            });
+
+            //taskWithResult.Wait();
+            var result = taskWithResult.Result;
+            PrintWithThread($"Result = {result}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
