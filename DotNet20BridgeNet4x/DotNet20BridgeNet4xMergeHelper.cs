1f1457690e7acdaf957cba66d10b6af067c1d3cd
RainsSoft <andyhebear@163.com>
2026-03-02 17:31:30 +0800

    Update DotNet20BridgeNet4xMergeHelper.cs
    .net dll程序集合并辅助

--- DotNet20BridgeNet4x/DotNet20BridgeNet4xMergeHelper.cs
+++ DotNet20BridgeNet4x/DotNet20BridgeNet4xMergeHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace DotNet20BridgeNet4x
@@ -42,15 +43,64 @@ namespace DotNet20BridgeNet4x
 
         //Note: for compatibility purposes, all options can be specified using '/', '-' or '--' prefix.
 
-        public static void MergeDll(string[] dlls) {
-/*
-cd ILRepack\bin\Release\netcoreapp3.1\
-dotnet ILRepack.dll /log /wildcards /internalize /out:..\..\netcoreapp3.1\ILRepack.dll /target:library ILRepack.dll BamlParser.dll Fasterflect.dll Mono.Cecil.dll Mono.Cecil.Mdb.dll Mono.Cecil.Pdb.dll Mono.Cecil.Rocks.dll runtimes\win-x64\lib\netstandard2.0\Mono.Posix.NETStandard.dll
-cd ..\net461
-ILRepack.exe /log /wildcards /internalize /out:..\..\net461\ILRepack.dll /target:library ILRepack.exe BamlParser.dll Fasterflect.dll Mono.Cecil.dll Mono.Cecil.Mdb.dll Mono.Cecil.Pdb.dll Mono.Cecil.Rocks.dll Mono.Posix.dll 
- */ 
+        public static void CreateCmd_MergeDll_ILRepack(string[] dlls,FileInfo ILRepackExe, string outputAssemblyFullName) {
+            /*
+            cd ILRepack\bin\Release\netcoreapp3.1\
+            dotnet ILRepack.dll /log /wildcards /internalize /out:..\..\netcoreapp3.1\ILRepack.dll /target:library ILRepack.dll BamlParser.dll Fasterflect.dll Mono.Cecil.dll Mono.Cecil.Mdb.dll Mono.Cecil.Pdb.dll Mono.Cecil.Rocks.dll runtimes\win-x64\lib\netstandard2.0\Mono.Posix.NETStandard.dll
+            cd ..\net461
+            ILRepack.exe /log /wildcards /internalize /out:..\..\net461\ILRepack.dll /target:library ILRepack.exe BamlParser.dll Fasterflect.dll Mono.Cecil.dll Mono.Cecil.Mdb.dll Mono.Cecil.Pdb.dll Mono.Cecil.Rocks.dll Mono.Posix.dll 
+             */
+            //合并dll
+            //ILRepack.exe / help
+            //ILRepack.exe / log:merge2.log / ndebug / wildcards /out:..\..\Bin_DLL\DotNet20BridgeNet4x.Net20.dll / target:library DotNet20BridgeNet4x.dll System.Mono.Posix.Net20.dll System.Core.Net20.dll System.Threading.Net20.dll
+            //pause
+            ILRepack.ILRepackConfig config = new ILRepack.ILRepackConfig() {
+                ILRepackExeFile = ILRepackExe,
+                 Log="ILRepack_MergeDLL.log",
+                 NDebug=true,
+                 Verbose=true,
+                 XmlDocs=true,
+            };
+            config.Libs = new List<DirectoryInfo>();
+            config.Libs.Add(new DirectoryInfo(new FileInfo( dlls[0]).DirectoryName));
+            ILRepack.ILRepackCmdBuilder builder = new ILRepack.ILRepackCmdBuilder(config);
+            List<FileInfo> refDlls = new List<FileInfo>();
+            for (int i = 1; i < dlls.Length; i++) {
+                refDlls.Add(new FileInfo(dlls[i]));
+            }
+            string cmdStr = builder.BuilderCmd(new FileInfo(outputAssemblyFullName), new FileInfo(dlls[0]), refDlls);
+            File.WriteAllText("ILRepack_CMD_"+new FileInfo(dlls[0]).Name+".bat",cmdStr,Encoding.ASCII);
+        }
+        public static void MergeDLL_ILMerge(string[] dlls, FileInfo ILMergeExe, string outputAssemblyFullName) {
+            // ILMerge.exe就是用于将多个程序集合并的工具。
+            // 官方下载地址: http://www.microsoft.com/en-us/download/details.aspx?id=17630
+            //            安装后可在命令提示符中使用。
+            // 命令内容：
+            // Usage: ilmerge[/ lib:directory] * [/ log[:filename]][/ keyfile:filename[/ delaysign]][/ internalize[:filename]][/ t[arget]:(library | exe | winexe)][/ closed][/ ndebug][/ ver:version][/ copyattrs[/ allowMultiple][/ keepFirst]][/ xmldocs][/ attr:filename]
+            // [/ targetplatform:< version >[,< platformdir >] | / v1 | / v1.1 | / v2 | / v4][/ useFullPublicKeyForReferences]
+            // [/ wildcards][/ zeroPeKind][/ allowDup:type] * [/ union][/ align:n]
+            // /out:filename < primary assembly > [< other assemblies > ...]
+            // 其中这两个是必须的参数：/out:filename < primary assembly >
+            // out:filename 表示输出的程序集名，< primary assembly > 表示输入的主程序集名。下面是一个实例：
+            // ilmerge.exe / t:winexe / targetplatform:v2 /out:main.new.exe main.exe Interop.IWshRuntimeLibrary.dll
+            // 表示将主程序集main.exe和另一个程序集Interop.IWshRuntimeLibrary.dll合并为main.new.exe，/ t:winexe表示生成目标是Windows应用程序，/ targetplatform:v2表示生成目标是.net 2.0程序集。
 
-     }
+            //合并dll
+            //ilmerge.exe /?
+            //ilmerge.exe / log : merge1.log / t:library / targetplatform:v2 / ndebug /out:DotNet20BridgeNet4x.Net20.dll DotNet20BridgeNet4x.dll System.Mono.Posix.Net20.dll System.Core.Net20.dll System.Threading.Net20.dll
+            //pause
+            ILMerge.ILMergeConfig config = new  ILMerge.ILMergeConfig() {
+                 ILMergeExeFile = ILMergeExe,
+                 Internalize=false
+            };
 
+            ILMerge.ILMergeCmdBuilder builder = new  ILMerge.ILMergeCmdBuilder(config);
+            List<FileInfo> refDlls = new List<FileInfo>();
+            for (int i = 1; i < dlls.Length; i++) {
+                refDlls.Add(new FileInfo(dlls[i]));
+            }
+            string cmdStr = builder.BuilderCmd(new FileInfo(outputAssemblyFullName), new FileInfo(dlls[0]), refDlls);
+            File.WriteAllText("ILMerge_CMD_" + new FileInfo(dlls[0]).Name + ".bat", cmdStr, Encoding.ASCII);
+        }
     }
 }
