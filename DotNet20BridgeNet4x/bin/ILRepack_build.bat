3222a3e99e7fc6f7c3d8b6ebb513f149c8d13efe
RainsSoft <andyhebear@163.com>
2026-03-02 17:29:14 +0800

    add ILRepack.exe

---
+++ DotNet20BridgeNet4x/bin/ILRepack_build.bat
@@ -0,0 +1,3 @@
+ILRepack.exe /help
+ILRepack.exe /log:merge2.log /ndebug /wildcards /out:..\..\Bin_DLL\DotNet20BridgeNet4x.Net20.dll /target:library DotNet20BridgeNet4x.dll System.Mono.Posix.Net20.dll System.Core.Net20.dll System.Threading.Net20.dll
+pause
